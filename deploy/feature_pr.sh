bb1dce601a391a1b0d2e7f2c997f200b5e1a6e09
Mark Zhang <mark.zhang@chanzuckerberg.com>
2019-09-30 17:07:49 -0700

    Add pr.sh

--- deploy/feature_pr.sh
+++ deploy/feature_pr.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-cur_branch=$(git branch | grep \* | cut -d ' ' -f2)
-git push origin $cur_branch
+DIRECTORY=`dirname $0`
 
-repository_url=$(git remote get-url origin | sed -e 's/git@//' -e 's/.git//' -e 's/:/\//')
-branch=$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-
-pr_url=https://$repository_url/compare/master...$branch
-
-open $pr_url
+# Pass arguments to pr.sh
+$DIRECTORY/pr.sh master ${@}
