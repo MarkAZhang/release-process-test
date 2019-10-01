30f1fc10f7d8cb5f44ddb8d0c36de18e6512d0b5
Mark Zhang <mark.zhang@chanzuckerberg.com>
2019-09-30 18:35:32 -0700

    Tweak to promote staging to prod.

--- deploy/promote_staging_to_prod.sh
+++ deploy/promote_staging_to_prod.sh
@@ -1,4 +1,4 @@
 # Get the directory that this script is in.
 echo "Deploying origin/staging to prod"
 DIRECTORY=`dirname $0`
-$DIRECTORY/core.sh staging prod --no-deploy
+$DIRECTORY/promote.sh staging prod --no-deploy
