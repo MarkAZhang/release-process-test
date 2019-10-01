c412a8694ab1b0b68692c6763134ba9c4ee5291e
Mark Zhang <mark.zhang@chanzuckerberg.com>
2019-09-30 18:45:41 -0700

    More changes to new.

--- deploy/new.sh
+++ deploy/new.sh
@@ -16,6 +16,15 @@ git fetch --all
 # It should always be possible to fast-forward master, staging, or prod, because you shouldn't commit anything directly those branches. 
 git rebase origin/$branch
 
+prefix=
+
+if [[ $entity_name == "release fix" ]]; then
+  prefix="release-fix-"
+fi
+if [[ $entity_name == "hot fix" ]]; then
+  prefix="hot-fix-"
+fi
+
 echo "Creating a new $entity_name branch off of ${branch}..."
-git co -b $branch_name
+git co -b $prefix$branch_name
 echo "Done!"
